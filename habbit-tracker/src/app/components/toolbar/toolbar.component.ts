7e244abbd1ce0a7bc57eae17530fc5261c2516e9
Ivan <igamanovich@elinext.com>
2022-09-27 12:04:42 +0300

    feat: add toolbar

--- habbit-tracker/src/app/components/toolbar/toolbar.component.ts
+++ habbit-tracker/src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IHabit} from "../habit";
 
 @Component({
   selector: 'app-toolbar',
@@ -11,5 +12,20 @@ export class ToolbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  public habits: IHabit[] = [
+    <IHabit>{
+      name: '15 Minute Walk',
+      frequency: 'Daily',
+      description:
+        'This habit makes my kitchen look nice and makes my day better the next morning.',
+    },
+    <IHabit>{
+      name: 'Weed the Garden',
+      frequency: 'Weekly',
+      description:
+        'The weeds get so out of hand if they wait any longer, and I like how nice our home looks with a clean lawn.',
+    },
+  ];
 
 }
